08eabf877c5f5116921d42711bcd2568be8e3a6a
kelvin8wong <kelvin8wong@gmail.com>
2017-11-03 03:10:52 +0000

    broadcast function implemented

--- server.js
+++ server.js
@@ -33,7 +33,12 @@ wss.on('connection', (socket) => {
     const newMessage = JSON.parse(message);
     console.log(`User ${newMessage.username} said ${newMessage.content}`);
     newMessage.id = uuidv4();
-    newMessagr.type= 'incomingMessage';
+    // if (newMessage.type === "postNotification"){
+    //   newMessage.type = "incomingNotification";
+    // } 
+    // if(newMessage.type === "postMessage"){
+    //   newMessage.type = "incomingMessage";
+    // } 
     broadcast(JSON.stringify(newMessage));
   })
 
